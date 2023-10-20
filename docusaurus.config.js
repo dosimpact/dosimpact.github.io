68603982cafe848f1f0faaf393fb5b81efa6b193
dosimpact <ypd03008@gmail.com>
2023-10-20 18:26:41 +0900

    bug-fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -128,15 +128,6 @@ const config = {
         style: "dark",
         links: [
           {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          {
             title: "Community",
             items: [
               {
